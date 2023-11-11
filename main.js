679436bc4c51ba511b091b407e89c56c7040ac52
nutrot777 <42739960+nutrot777@users.noreply.github.com>
2023-11-11 18:36:32 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, // Required for communication with renderer process
+    },
+  });
+
+  const startUrl = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+
+  mainWindow.loadURL(startUrl);
+  mainWindow.on('closed', () => (mainWindow = null));
+
+  // Handle 'go-back' event from renderer process
+  ipcMain.on('go-back', () => {
+    if (mainWindow && mainWindow.webContents.canGoBack()) {
+      mainWindow.webContents.goBack();
+    }
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
